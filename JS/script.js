fb9e510d23d84a5c4da55475d513b603a68071d6
Carlos Iguaro <carlosiguaro9@gmail.com>
2022-10-03 20:22:48 -0300

    Primera revision, descripcion de algunos conecptos apuntes y recursos utiles.

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,21 @@
-const boton = document.querySelector('.boton');
-const enlaces = document.querySelector('.enlaces-menu');
-const barras = document.querySelectorAll('.boton span');
+/*
+    Cuando se hacen referencias a elementos del DOM
+    debes escuchar los eventos ya sea ""load"" o ""DOMContentLoaded""
+    que indican a partir de que momentos esos elemtnso estan diponible
 
-boton.addEventListener('click', () => {
-    enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
+    En este caso:
+    const boton = document.querySelector('.boton');
+    const enlaces = document.querySelector('.enlaces-menu');
+    const barras = document.querySelectorAll('.boton span');
+*/
+
+window.addEventListener('DOMContentLoaded', function() {
+    const boton = document.querySelector('.boton');
+    const enlaces = document.querySelector('.enlaces-menu');
+    const barras = document.querySelectorAll('.boton span');
+    
+    boton.addEventListener('click', () => {
+        enlaces.classList.toggle('activado');
+        barras.forEach(child => {child.classList.toggle('animado')});
+    });
 });
